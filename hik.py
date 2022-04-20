c51d1ebe86e74a41ff4e224d7625784190fb5942
Edwin Eefting <edwin@datux.nl>
2022-04-20 11:27:41 +0200

    initial commit

--- hik.py
+++ hik.py
@@ -1,5 +1,7 @@
 from hcnetsdk import HCNetSDK, NET_DVR_DEVICEINFO_V30, NET_DVR_DEVICEINFO_V30, NET_DVR_SETUPALARM_PARAM, fMessageCallBack, COMM_ALARM_V30, COMM_ALARM_VIDEO_INTERCOM, NET_DVR_VIDEO_INTERCOM_ALARM, NET_DVR_ALARMINFO_V30, ALARMINFO_V30_ALARMTYPE_MOTION_DETECTION, VIDEO_INTERCOM_ALARM_ALARMTYPE_DOORBELL_RINGING, VIDEO_INTERCOM_ALARM_ALARMTYPE_DISMISS_INCOMING_CALL, VIDEO_INTERCOM_ALARM_ALARMTYPE_TAMPERING_ALARM, VIDEO_INTERCOM_ALARM_ALARMTYPE_DOOR_NOT_CLOSED, COMM_UPLOAD_VIDEO_INTERCOM_EVENT, NET_DVR_VIDEO_INTERCOM_EVENT, VIDEO_INTERCOM_EVENT_EVENTTYPE_UNLOCK_LOG, VIDEO_INTERCOM_EVENT_EVENTTYPE_ILLEGAL_CARD_SWIPING_EVENT, NET_DVR_UNLOCK_RECORD_INFO
 from ctypes import POINTER, cast, c_char_p, c_byte
+import config
+import paho.mqtt.client as mqtt
 
 def callback(command: int, alarmer_pointer, alarminfo_pointer, buffer_length, user_pointer):
     if (command == COMM_ALARM_V30):
@@ -7,6 +9,7 @@ def callback(command: int, alarmer_pointer, alarminfo_pointer, buffer_length, us
             alarminfo_pointer, POINTER(NET_DVR_ALARMINFO_V30)).contents
         if (alarminfo_alarm_v30.dwAlarmType == ALARMINFO_V30_ALARMTYPE_MOTION_DETECTION):
             print(f"Motion detected")
+            client.publish(config.mqtt_topic+"/motion")
         else:
             print(
                 f"COMM_ALARM_V30, unhandled dwAlarmType: {alarminfo_alarm_v30.dwAlarmType}")
@@ -15,12 +18,16 @@ def callback(command: int, alarmer_pointer, alarminfo_pointer, buffer_length, us
             alarminfo_pointer, POINTER(NET_DVR_VIDEO_INTERCOM_ALARM)).contents        
         if (alarminfo_alarm_video_intercom.byAlarmType == VIDEO_INTERCOM_ALARM_ALARMTYPE_DOORBELL_RINGING):
             print("Doorbell ringing")
+            client.publish(config.mqtt_topic+"/ringing")
         elif (alarminfo_alarm_video_intercom.byAlarmType == VIDEO_INTERCOM_ALARM_ALARMTYPE_DISMISS_INCOMING_CALL):
             print("Call dismissed")            
+            client.publish(config.mqtt_topic+"/dismissed")
         elif (alarminfo_alarm_video_intercom.byAlarmType == VIDEO_INTERCOM_ALARM_ALARMTYPE_TAMPERING_ALARM):
             print("Tampering alarm")
+            client.publish(config.mqtt_topic+"/tamper")
         elif (alarminfo_alarm_video_intercom.byAlarmType == VIDEO_INTERCOM_ALARM_ALARMTYPE_DOOR_NOT_CLOSED):
             print("Door not closed")
+            client.publish(config.mqtt_topic+"/notclosed")
         else:
             print(
                 f"COMM_ALARM_VIDEO_INTERCOM, unhandled byAlarmType: {alarminfo_alarm_video_intercom.byAlarmType}")
@@ -29,8 +36,12 @@ def callback(command: int, alarmer_pointer, alarminfo_pointer, buffer_length, us
             alarminfo_pointer, POINTER(NET_DVR_VIDEO_INTERCOM_EVENT)).contents
         if (alarminfo_upload_video_intercom_event.byEventType == VIDEO_INTERCOM_EVENT_EVENTTYPE_UNLOCK_LOG):          
             print(f"Unlocked by: {list(alarminfo_upload_video_intercom_event.uEventInfo.struUnlockRecord.byControlSrc)}")
+            card_id=bytes(alarminfo_upload_video_intercom_event.uEventInfo.struUnlockRecord.byControlSrc).hex()
+            client.publish(config.mqtt_topic+"/unlocked", card_id)
+
         elif (alarminfo_upload_video_intercom_event.byEventType == VIDEO_INTERCOM_EVENT_EVENTTYPE_ILLEGAL_CARD_SWIPING_EVENT):
             print(f"Illegal card swiping")
+            client.publish(config.mqtt_topic+"/illegalcard")
         else:
             print(
                 f"COMM_ALARM_VIDEO_INTERCOM, unhandled byEventType: {alarminfo_upload_video_intercom_event.byEventType}")
@@ -41,9 +52,9 @@ def callback(command: int, alarmer_pointer, alarminfo_pointer, buffer_length, us
 HCNetSDK.NET_DVR_Init()
 HCNetSDK.NET_DVR_SetValidIP(0, True)
 
-device_info = NET_DVR_DEVICEINFO_V30()
+# device_info = NET_DVR_DEVICEINFO_V30()
 
-user_id = HCNetSDK.NET_DVR_Login_V30( "192.168.1.206".encode('utf-8'), 8000, "admin".encode('utf-8'), "Password123".encode('utf-8'), device_info)
+user_id = HCNetSDK.NET_DVR_Login_V30( config.intercom_host.encode('utf-8'), 8000, config.intercom_user.encode('utf-8'), config.intercom_pass.encode('utf-8'))
 
 if (user_id < 0):
     print(
@@ -69,8 +80,32 @@ if (alarm_handle < 0):
 message_callback = fMessageCallBack(callback)
 HCNetSDK.NET_DVR_SetDVRMessageCallBack_V50(0, message_callback, user_id)
 
-input("Press Enter to exit...\n")
 
-HCNetSDK.NET_DVR_CloseAlarmChan_V30(alarm_handle)
-HCNetSDK.NET_DVR_Logout_V30(user_id)
-HCNetSDK.NET_DVR_Cleanup()
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("MQTT connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    # client.subscribe("$SYS/#")
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+if (config.mqtt_user!=""):
+    client.username_pw_set(config.mqtt_user, config.mqtt_pass)
+client.connect(config.mqtt_host, 1883, 60)
+
+client.loop_forever()
+
+
+#never called anyway..
+# HCNetSDK.NET_DVR_CloseAlarmChan_V30(alarm_handle)
+# HCNetSDK.NET_DVR_Logout_V30(user_id)
+# HCNetSDK.NET_DVR_Cleanup()
